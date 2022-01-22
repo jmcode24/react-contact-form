0ec6e5d705db1b28f06b7a4753b892caeee9df4b
Jamil Mohammed <zeus2tee@gmail.com>
2022-01-22 06:54:52 +0000

    added a font bolder class

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -12,9 +12,9 @@ const Contacts = (props) => {
                 <div key={index}>
                   <div class="card mt-1 mb-1 text-center bg-primary">
                     <div class="card-body">
-                      <h5 className="text-white"><span className="text-secondary">Name:</span> {details.name}</h5>
-                      <h5 className="text-white"><span className="text-warning">Phone Number:</span> {details.phone}</h5>
-                      <h5 className="text-white"><span className="text-success">Location:</span> {details.location}</h5>
+                      <h5 className="text-white"><span className="text-secondary font-weight-bolder">Name:</span> {details.name}</h5>
+                      <h5 className="text-white"><span className="text-warning font-weight-bolder">Phone Number:</span> {details.phone}</h5>
+                      <h5 className="text-white"><span className="text-success font-weight-bolder">Location:</span> {details.location}</h5>
                     </div>
                   </div>
                 </div>
